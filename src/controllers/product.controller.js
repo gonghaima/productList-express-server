91a76aa79452974d7fd435a8a8859ebc18d62704
Steven <gonghaima@hotmail.com>
2019-06-16 09:36:12 +1000

    use axios to request data, replacoing mock

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,14 +1,10 @@
 import HTTPStatus from "http-status";
-import productAPIRequest from "../mock/productsData";
 import productModel from "../models/product.model";
 
 const getAll = async (req, res, next) => {
   try {
-    console.log("before calling productAPIRequest");
-
     const products = await productModel.findAll();
-
-    return res.status(HTTPStatus.FOUND).json(products);
+    return res.status(HTTPStatus.FOUND).json(products.data);
   } catch (e) {
     e.status = HTTPStatus.BAD_REQUEST;
     return next(e);
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,7 +1,9 @@
 import productAPIRequest from "../mock/productsData";
-
+import axios from "axios";
 const findAll = async () => {
-  const products = await productAPIRequest();
+  const products = await axios.get(
+    "https://raw.githubusercontent.com/gonghaima/data/master/products.json"
+  );
   return products;
 };
 
