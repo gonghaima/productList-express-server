45345426a5e77b10c4248e61b7525bf3d3fad73f
Steven <gonghaima@hotmail.com>
2019-06-17 14:58:43 +1000

    remove 'all' in routing

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 import ProductController from "../controllers/product.controller";
 
 const routes = new Router();
-routes.get("/all", ProductController.getAll);
+routes.get("/", ProductController.getAll);
 export default routes;
