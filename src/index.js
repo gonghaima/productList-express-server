23f3e05dd69b9d445b1c027c40bcc8bef7239656
Steven <gonghaima@hotmail.com>
2019-06-20 10:27:36 +1000

    show version number at route /version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
+import process from "process";
 import apiRoutes from "./routes";
 
 const app = express();
@@ -7,6 +10,14 @@ app.use("/api", apiRoutes);
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function(req, res) {
   res.send("hello world");
+});
+
+// respond with version number defined in package.json
+app.get("/version", function(req, res) {
+  const version = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), "package.json"), "utf8")
+  ).version;
+  res.status(200).send(version);
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
