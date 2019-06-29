6012985da81286a1626b711a280aa773b7e6ee1f
Steven <gonghaima@hotmail.com>
2019-06-29 11:02:00 +1000

    TODO - mock redis client and axios

---
+++ src/models/product.model.spec.js
@@ -0,0 +1,6 @@
+import { findAll } from "./product.model";
+describe("product model", () => {
+  it("should use findAll function to get all products", () => {
+    expect(true).toBe(true);
+  });
+});
