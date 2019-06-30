42758a3d9cfb1f0baeb3782fd0c84f7f94a48359
Steven <gonghaima@hotmail.com>
2019-06-30 13:30:42 +1000

    product model test added - TODO: coverage, try mock directly without dependency injection

--- src/models/product.model.spec.js
+++ src/models/product.model.spec.js
@@ -1,6 +1,24 @@
-import { findAll } from "./product.model";
+import productModel from "./product.model";
+import redis from "redis";
+// const { promisify } = require("util");
+import redisClient from "../services/redis-client";
+
+jest.mock("redis", () => ({
+  createClient: () => Promise.resolve(true)
+}));
+jest.mock("../services/redis-client", () => ({
+  getAsync: () => {},
+  setAsync: () => {},
+  keysAsync: () => {}
+}));
+
 describe("product model", () => {
-  it("should use findAll function to get all products", () => {
-    expect(true).toBe(true);
+  it("should ok", async done => {
+    const modelData = await productModel.findAll({
+      setAsync: () => Promise.resolve(true),
+      getAsync: () => Promise.resolve(JSON.stringify(["product1", "product2"]))
+    });
+    expect(modelData).toEqual(["product1", "product2"]);
+    done();
   });
 });
