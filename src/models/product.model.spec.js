603f036965620f8d6df9667af9d8a3d0fb0ebce3
Steven <gonghaima@hotmail.com>
2019-07-01 09:17:56 +1000

    clean up

--- src/models/product.model.spec.js
+++ src/models/product.model.spec.js
@@ -13,11 +13,8 @@ jest.mock("../services/redis-client", () => ({
 }));
 
 describe("product model", () => {
-  it("should ok", async () => {
-    const modelData = await productModel.findAll({
-      setAsync: () => jest.fn(),
-      getAsync: () => Promise.resolve(JSON.stringify(["product1", "product2"]))
-    });
+  it("should get product data successfully", async () => {
+    const modelData = await productModel.findAll();
 
     expect(modelData).toEqual(["product3", "product4"]);
     expect(modelData.length).toEqual(2);
