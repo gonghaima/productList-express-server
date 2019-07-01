ce4b4c20fd5ec11fad5fcb2f91f9d104a08ddf64
Steven <gonghaima@hotmail.com>
2019-07-01 16:01:29 +1000

    product controller unit test - initial setup

---
+++ src/controllers/product.controller.spec.js
@@ -0,0 +1,8 @@
+describe("product.controller", () => {
+  it("should use get function to return specified data", () => {
+    expect(true).toBe(true);
+  });
+  it("should use getAll function to return all data", () => {
+    expect(true).toBe(true);
+  });
+});
