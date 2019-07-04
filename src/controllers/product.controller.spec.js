8d2928bcda0a3fea139fc2e6a1433feb73de00f5
Steven <gonghaima@hotmail.com>
2019-07-05 03:26:04 +1000

    unit test - productController get

--- src/controllers/product.controller.spec.js
+++ src/controllers/product.controller.spec.js
@@ -53,4 +53,27 @@ describe("product.controller", () => {
     );
     expect(allProductData.products[1].id).toEqual(1000);
   });
+
+  it("get function should return paginated data", async () => {
+    const mockResponse = () => {
+      const res = {};
+      res.status = () => res;
+      res.json = data => {
+        return { ...res, ...data };
+      };
+      return res;
+    };
+    const mockRequest = () => {
+      return { query: { offset: 1, limit: 1 } };
+    };
+    const mkReq = jest.fn(mockRequest);
+    const mkRes = jest.fn(mockResponse);
+    const mkNext = jest.fn();
+    const productData = await productController.get(mkReq(), mkRes(), mkNext);
+    expect(productData.products.length).toEqual(1);
+    expect(productData.products[0].description).toEqual(
+      "evolve customized technologies"
+    );
+    expect(productData.products[0].id).toEqual(1000);
+  });
 });
